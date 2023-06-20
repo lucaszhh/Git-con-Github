427daa2663fd2276f4eff2ed7585683a89574d68
Lucas Zarandón <lucaszarandon24@gmail.com>
2023-06-20 00:54:53 -0300

    add view github, responsive footer and implement arrowtopfunction

---
+++ scripts/headerFooter.js
@@ -0,0 +1,43 @@
+const footer = () => {
+    document.querySelector("footer").innerHTML = `
+    <article class="title">
+        <h2 class="titleText">
+            <i class="fa-brands fa-git-alt"></i>
+            Has llegado al final de la página!
+            <i class="fa-brands fa-git-alt"></i>
+        </h2>
+    </article>
+    <article class="titleText github">
+        <h2><i class="fa-brands fa-github"></i></h2>
+        <a href="https://github.com/lucaszhh" target="_blank" class="download">
+            <div>
+                <h3>visita mi perfil de GitHub</h3>
+                <h2>lucaszhh</h2>
+            </div>
+        </a>
+        <h2><i class="fa-brands fa-github"></i></h2>
+    </article>
+`
+};
+
+const header = () => {
+    document.querySelector(".headerView").innerHTML += `
+    <div>
+        <img src="../images/icons/gitIcon.png" alt="Git icono">
+        <span>+</span>
+        <img src="../images/icons/githubIcon.png" alt="GitHub icono">
+    </div>
+    <article class="info-card">
+        <a href="../index.html">
+            <img src="../images/icons/gitIcon.png" />
+            <h2>Inicio</h2>
+            <img src="../images/icons/gitHubIcon.png" />
+        </a>
+    </article>
+    `
+}
+
+
+
+footer();
+header();
---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const arrowUp = () => {
+    document.querySelector("body").innerHTML += `
+        <a href="#top"><i title="Ir arriba" class="fa-solid fa-circle-up fa-beat arrowUp"></i></a>
+    `
+};
+
+arrowUp();
